9ecedd18cdfe5780989736490f116a9870b7d35f
Brendah@90 <bregamah@gmail.com>
2023-08-24 10:37:16 -0700

    builtin.c

--- builtin.c
+++ builtin.c
@@ -1,15 +1,41 @@
 #include "shell.h"
 
 /**
- * exit_bul - exit statue shell
- * @cmd: parsed command
- * @input: user input
- * @argv:program name
- * @c:execute count
- * Return: void (exit statue)
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
+int _myexit(info_t *info)
+{
+	int exitcheck;
 
-void  exit_bul(char **cmd, char *input, char **argv, int c)
+	if (info->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
